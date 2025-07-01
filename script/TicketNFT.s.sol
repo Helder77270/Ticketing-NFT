c415121f373819594d2edd7f9b4db2f1a6724af9
Helder77270 <helder.salvador@yahoo.com>
2025-07-02 01:26:49 +0200

    :zap: On va y arriver

--- script/TicketNFT.s.sol
+++ script/TicketNFT.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
 import "openzeppelin-contracts-upgradeable/proxy/ERC1967/ERC1967Proxy.sol";
-import "src/TicketNFT.sol";
+import "../src/TicketNFT.sol";
 
 contract DeployTicketNFT is Script {
     function run() external {
